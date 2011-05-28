5123f531a1b553faacb5e575cb3082aafc44d3ba
exk0730@gmail.com <exk0730@gmail.com@1ad94d22-6a4f-7c32-80d2-01e4afe3d47e>
2011-05-28 01:28:19 +0000

    Removed EntityG class, since it's not needed. New implementation is in AbstractEntityG. Changed javadocs of CFDL.

--- trunk/src/edu/rit/entityg/prefuse/view/CustomizedForceDirectedLayout.java
+++ trunk/src/edu/rit/entityg/prefuse/view/CustomizedForceDirectedLayout.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.rit.entityg.prefuse.view;
 
 import prefuse.action.layout.graph.ForceDirectedLayout;
