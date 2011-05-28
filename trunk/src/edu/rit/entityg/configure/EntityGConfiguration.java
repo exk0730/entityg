943bee8b7b6fa7d09dceb95a052e1bd53277978e
exk0730@gmail.com <exk0730@gmail.com@1ad94d22-6a4f-7c32-80d2-01e4afe3d47e>
2011-05-28 16:03:21 +0000

    Code cleanup. Renamed methods, moved things around, etc. in EntityG configuration classes. This project is almost complete. I still need to review all the code and make sure no pieces are missing.

--- trunk/src/edu/rit/entityg/configure/EntityGConfiguration.java
+++ trunk/src/edu/rit/entityg/configure/EntityGConfiguration.java
@@ -2,6 +2,7 @@ package edu.rit.entityg.configure;
 
 import edu.rit.entityg.AbstractEntityG;
 import edu.rit.entityg.DatabaseEntityG;
+import edu.rit.entityg.dataloaders.DataSourceType;
 import edu.rit.entityg.exceptions.InvalidIniException;
 import edu.rit.entityg.utils.ExceptionUtils;
 import java.lang.reflect.Method;
@@ -9,34 +10,60 @@ import java.lang.reflect.Method;
 import static edu.rit.entityg.configure.EntityGOptions.*;
 
 /**
+ * This class is used for configuring an {@link AbstractEntityG} based on command-line arguments and/or a configuration
+ * file.
  * @date May 24, 2011
  * @author Eric Kisner
  */
-//TODO: fix this class. durp.
 public class EntityGConfiguration {
 
     private EntityGIniFile entitygINI;
     private EntityGCommandLine entitygCL;
     private AbstractEntityG entityG;
-    private boolean emptyArgs = true;
+    private boolean emptyArgs;
 
+    /**
+     * Default constructor. Should be used when we want to run EntityG within an application, and/or with the default
+     * configuration file.
+     * @throws InvalidIniException
+     */
     public EntityGConfiguration() throws InvalidIniException {
-        doShit( new String[]{}, null );
+        configAll( new String[]{}, null );
     }
 
+    /**
+     * Standalone EntityG constructor. Should be used when we have arguments to pass into EntityG. This constructor
+     * will also check to see if a non-default configuration file has been specified within the arguments.
+     * @param args The command-line arguments passed into a main method.
+     * @throws InvalidIniException
+     */
     public EntityGConfiguration( String[] args ) throws InvalidIniException {
-        doShit( args, null );
+        configAll( args, null );
     }
 
+    /**
+     * Constructor that should be used when we are specifying a different configuration file. This constructor relies
+     * on the configuration file to set up EntityG.
+     * @param iniFile The <code>entityg.ini</code> file's location.
+     * @throws InvalidIniException
+     */
     public EntityGConfiguration( String iniFile ) throws InvalidIniException {
-        doShit( new String[]{}, iniFile );
+        configAll( new String[]{}, iniFile );
     }
 
+    /**
+     * Calling this method will start an {@link AbstractEntityG} graph.
+     */
     public void startEntityG() {
         entityG.start();
     }
 
-    public final void doShit( String[] args, String filePath ) throws InvalidIniException {
+    /**
+     * Sets up any required configuration objects based on the command-line arguments or a configuration file.
+     * @param args The command-line arguments passed into a main method.
+     * @param filePath The path to the configuration file.
+     */
+    private void configAll( String[] args, String filePath ) throws InvalidIniException {
         emptyArgs = !(args.length > 0);
 
         if( !emptyArgs ) {
@@ -44,7 +71,11 @@ public class EntityGConfiguration {
             if( entitygCL.optionExists( CONFIG_FILE ) ) {
                 entitygINI = new EntityGIniFile( entitygCL.getOptionValue( CONFIG_FILE ), emptyArgs );
             }
-        } else {
+        }
+
+        //If an option for a config file wasn't set in the command line arguments, we still need to create an object
+        //to handle any configuration file options.
+        if( entitygINI == null ) {
             if( filePath != null ) {
                 entitygINI = new EntityGIniFile( filePath, emptyArgs );
             } else {
@@ -56,17 +87,31 @@ public class EntityGConfiguration {
 
         if( dst.equalsIgnoreCase( "database" ) ) {
             entityG = new DatabaseEntityG();
-            doSomeMoreShit();
+            runMethodsForDataSource( DataSourceType.DATABASE );
             entityG.connectToDatabase();
+        } else if( dst.equalsIgnoreCase( "csv" ) ) {
+            throw new UnsupportedOperationException( "CSV is not implemented yet." );
+        } else if( dst.equalsIgnoreCase( "xml" ) ) {
+            throw new UnsupportedOperationException( "XML is not implemented yet." );
+        } else {
+            throw new IllegalStateException( dst + " is not a supported data source type." );
         }
     }
 
-    private void doSomeMoreShit() {
+    /**
+     * Runs any setter methods based on the configuration objects. This method will first retrieve any options that can
+     * be set based on <code>dst</code>, then run any setter methods based on the names of those options using
+     * reflection.
+     * @param dst The {@link DataSourceType} that we are setting fields for.
+     */
+    private void runMethodsForDataSource( DataSourceType dst ) {
         try {
-            for( String option : getDatabaseOptions() ) {
+            for( String option : getOptions( dst ) ) {
                 Method m = getMethodFromOption( option );
                 m.invoke( entityG, getValue( option ) );
             }
+        } catch( IllegalArgumentException iae ) {
+            ExceptionUtils.handleException( iae );
         } catch( Exception e ) {
             ExceptionUtils.handleException( e );
         }
@@ -82,6 +127,14 @@ public class EntityGConfiguration {
         }
     }
 
+    /**
+     * Retrieves a {@link Method} from <code>optionName</code>. These methods should be setters that start with
+     * <code>set_</code> and then end with <code>optionName</code>. The methods will be retrieved from the class
+     * {@link AbstractEntityG}.
+     * @param optionName The name of the option we are running a setter method for.
+     * @return The {@link Method} whose name is equal to <code>set_[optionName]</code> or null, if <code>optionName
+     *         </code> could not be found.
+     */
     private Method getMethodFromOption( String optionName ) {
         final String prefix = "set_";
         for( Method m : AbstractEntityG.class.getMethods() ) {
@@ -92,10 +145,25 @@ public class EntityGConfiguration {
         return null;
     }
 
+    /**
+     * Returns the value of an option which has been specified in either the command-line arguments, or within the
+     * configuration file.
+     * @param optionName The option that we want to get a value for.
+     * @return The value of <code>optionName</code> if it was found in the command-line arguments or configuration file,
+     *         or an empty string if none could be found.
+     */
     private String getValue( String optionName ) {
+        /**
+         * In order to properly retrieve a value from the command-line arguments or configuration file, we need to first
+         * check that the arguments are not empty. If the args aren't empty, and they contain <code>optionName</code>,
+         * then return that value, else, try looking in the config file for the option. If the args are empty, only
+         * look in the config file for the value of <code>optionName</code>.
+         */
         if( !emptyArgs ) {
             if( entitygCL.optionExists( optionName ) ) {
                 return entitygCL.getOptionValue( optionName );
+            } else if( entitygINI.optionExists( optionName ) ) {
+                return entitygINI.getOptionValue( optionName );
             }
         } else {
             if( entitygINI.optionExists( optionName ) ) {
