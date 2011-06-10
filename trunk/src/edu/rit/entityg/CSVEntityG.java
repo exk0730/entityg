9c5087695a1c6b9e82814ba677e05fefba19aa97
exk0730@gmail.com <exk0730@gmail.com@1ad94d22-6a4f-7c32-80d2-01e4afe3d47e>
2011-06-10 03:44:45 +0000

    Implemented CSVEntityG. It is now possible to load data from a CSV file. I have not implemented this into the configuration classes, however, I have added hard-code to EntityGMain to test that CSVEntityG works correctly. Some javadoc changes to come on CSVConnection, and then I can start working on implementing the configuration side of CSV loading.

--- trunk/src/edu/rit/entityg/CSVEntityG.java
+++ trunk/src/edu/rit/entityg/CSVEntityG.java
@@ -2,9 +2,13 @@ package edu.rit.entityg;
 
 import edu.rit.entityg.csv.CSVConnection;
 import edu.rit.entityg.dataloaders.CSVLoader;
+import edu.rit.entityg.exceptions.BadSetupException;
 import edu.rit.entityg.treeimpl.GenericTreeNode;
+import edu.rit.entityg.utils.ExceptionUtils;
 import java.awt.event.MouseEvent;
 import java.util.HashMap;
+import javax.swing.SwingUtilities;
+import prefuse.data.Node;
 import prefuse.visual.VisualItem;
 
 import static edu.rit.entityg.csv.CSVConnection.DELIM;
@@ -19,6 +23,7 @@ import static edu.rit.entityg.csv.CSVConnection.DELIM;
 public class CSVEntityG extends AbstractEntityG {
 
     private String fileName;
+    private String centerColumnName;
     private int centerColumnNumber;
     private int[] infoColumnNumbers;
     private HashMap<Integer, String> columnToNameMapping;
@@ -31,12 +36,66 @@ public class CSVEntityG extends AbstractEntityG {
         super();
     }
 
-    public GenericTreeNode<String> setupAbsoluteParent() {
-        throw new UnsupportedOperationException( "Not supported yet." );
+    /**
+     * Sets the center node column name.
+     * @param columnName The name of the column in the CSV file that contains center node data.
+     */
+    public void set_center_node_column_name( String columnName ) {
+        this.centerColumnName = columnName;
     }
 
-    public void customItemClicked( VisualItem item, MouseEvent e ) {
-        throw new UnsupportedOperationException( "Not supported yet." );
+    /**
+     * Set the path to the CSV file.
+     * @param fileName The CSV file's path.
+     */
+    public void set_file_name( String fileName ) {
+        this.fileName = fileName;
+    }
+
+    /**
+     * Set the column number that contains center node data.
+     * @param columnNumber An integer as a String that represents the column index.
+     */
+    public void set_center_node_column_number( String columnNumber ) {
+        try {
+            this.centerColumnNumber = Integer.parseInt( columnNumber );
+        } catch( NumberFormatException nfe ) {
+            ExceptionUtils.handleMessage( columnNumber + " is not a number." );
+            this.centerColumnNumber = -1;
+        }
+    }
+
+    /**
+     * Sets the list of column numbers which contain all information node data.
+     * @param columnNumbers A list of integers, represented as a String with {@link CSVConnection#DELIM} separating
+     *                      each number.
+     */
+    public void set_information_node_column_numbers( String columnNumbers ) {
+        String[] temp = columnNumbers.split( DELIM );
+        this.infoColumnNumbers = new int[temp.length];
+        for( int i = 0; i < temp.length; i++ ) {
+            try {
+                infoColumnNumbers[i] = Integer.parseInt( temp[i] );
+            } catch( NumberFormatException nfe ) {
+                ExceptionUtils.handleMessage( temp[i] + " is not a number." );
+                this.infoColumnNumbers = new int[0];
+                break;
+            }
+        }
+    }
+
+    /**
+     * Sets the column names to each column in the CSV file.
+     * @param columnNames A list of Strings, with a {@link CSVConnection#DELIM} separating each header. Also, it is
+     *                    important that these column names are in the same order as the columns in the CSV file. All
+     *                    columns must have headers / must be represented somehow.
+     */
+    public void set_column_to_name_mapping( String columnNames ) {
+        columnToNameMapping = new HashMap<Integer, String>();
+        String[] temp = columnNames.split( DELIM );
+        for( int i = 0; i < temp.length; i++ ) {
+            columnToNameMapping.put( i, temp[i] );
+        }
     }
 
     public void connectToDataSource() {
@@ -45,27 +104,54 @@ public class CSVEntityG extends AbstractEntityG {
         super.registerLoader( loader );
     }
 
-    public void set_file_name( String fileName ) {
-        this.fileName = fileName;
-    }
-
-    public void set_center_node_column_number( String columnNumber ) {
-        this.centerColumnNumber = Integer.parseInt( columnNumber );
-    }
-
-    public void set_information_node_column_numbers( String columnNumbers ) {
-        String[] temp = columnNumbers.split( DELIM );
-        this.infoColumnNumbers = new int[temp.length];
-        for( int i = 0; i < temp.length; i++ ) {
-            infoColumnNumbers[i] = Integer.parseInt( temp[i] );
+    public GenericTreeNode<String> setupAbsoluteParent() {
+        loader.setCenterNodeColumnNumber( centerColumnNumber );
+        loader.setColumnToNameMapping( columnToNameMapping );
+        loader.setInformationNodeColumnNumbers( infoColumnNumbers );
+        try {
+            GenericTreeNode<String> ret = loader.loadAbsoluteParent( centerColumnName );
+            return ret;
+        } catch( BadSetupException bse ) {
+            ExceptionUtils.handleException( bse );
+            throw new RuntimeException( bse );
         }
     }
 
-    public void set_column_to_name_mapping( String columnNames ) {
-        columnToNameMapping = new HashMap<Integer, String>();
-        String[] temp = columnNames.split( DELIM );
-        for( int i = 0; i < temp.length; i++ ) {
-            columnToNameMapping.put( i, temp[i] );
+    public void customItemClicked( VisualItem item, MouseEvent e ) {
+        if( !SwingUtilities.isLeftMouseButton( e ) ) return;
+        if( e.getClickCount() == 2 ) {//DoubleClick
+            //The backing Tuple of this visual item is actually a Node object (from g.addNode)
+            Node source = (Node) item.getSourceTuple();
+            //Get the related TreeNode of this Node
+            GenericTreeNode<String> treeNode = displayNodeToDataNodeMap.get( source );
+            /**
+             * If the Tree node has children, and they are visible nodes on the graph, we want to set those
+             * nodes to be invisible. Else, if the tree node has children and they are invisible, we want
+             * to set those nodes to be visible.
+             */
+            if( treeNode.hasChildren() ) {
+                if( hasVisibleChildren( item ) ) {
+                    setVisibilityOfAllChildren( item, false );
+                } else {
+                    setVisibilityOfAllChildren( item, true );
+                }
+            } else {
+                //If they click on a "center node"
+                try {
+                    if( treeNode.isCenterNode() ) {
+                        treeNode = loader.loadInformationNodes( treeNode, treeNode.getData() );
+                    } else {    //Else they clicked on an information node
+                        treeNode = loader.loadCenterNodes( treeNode, defaultMaxNodes, treeNode.getData(),
+                                                           treeNode.getDataHeader() );
+                    }
+                } catch( BadSetupException bse ) {
+                    ExceptionUtils.handleException( bse );
+                }
+                //Retrieve all children of this TreeNode and render it on the graph.
+                if( treeNode.hasChildren() ) {
+                    renderNewNodes( source, treeNode );
+                }
+            }
         }
     }
 }
--- trunk/src/edu/rit/entityg/dataloaders/CSVLoader.java
+++ trunk/src/edu/rit/entityg/dataloaders/CSVLoader.java
@@ -1,12 +1,24 @@
 package edu.rit.entityg.dataloaders;
 
 import edu.rit.entityg.csv.CSVConnection;
+import edu.rit.entityg.csv.TableRow;
 import edu.rit.entityg.exceptions.BadSetupException;
 import edu.rit.entityg.treeimpl.GenericTreeNode;
+import edu.rit.entityg.utils.ExceptionUtils;
 import java.io.IOException;
 import java.util.HashMap;
 
 /**
+ * Implements {@link DataSourceLoader} for a {@link DataSourceType#CSV} data source. The following
+ * represents what information is needed prior to loading data into EntityG from a CSV file:
+ * <ul>
+ * <li>The file name that we're loading data from.</li>
+ * <li>The column number that we want center node's to take data from.</li>
+ * <br/>
+ * <li>The column names of each column.</li>
+ * <br/>
+ * <li>The information nodes column numbers.</li>
+ * </ul>
  * @date Jun 4, 2011
  * @author Eric Kisner
  */
@@ -15,7 +27,7 @@ public class CSVLoader implements DataSourceLoader {
     private CSVConnection conn;
     private HashMap<Integer, String> columnToColumnNameMapping;
     private int centerNodeColumnNumber;
-    private int [] informationNodeColumnNumbers;
+    private int[] informationNodeColumnNumbers;
 
     /**
      * Default constructor.
@@ -30,29 +42,132 @@ public class CSVLoader implements DataSourceLoader {
         conn.close();
     }
 
-    public GenericTreeNode<String> loadAbsoluteParent( Object data ) throws BadSetupException {
-        throw new UnsupportedOperationException( "Not supported yet." );
-    }
-
-    public GenericTreeNode<String> loadInformationNodes( GenericTreeNode<String> parent, Object... data )
-            throws BadSetupException {
-        throw new UnsupportedOperationException( "Not supported yet." );
-    }
-
-    public GenericTreeNode<String> loadCenterNodes( GenericTreeNode<String> parent, int maxNodes, Object... data )
-            throws BadSetupException {
-        throw new UnsupportedOperationException( "Not supported yet." );
-    }
-
+    /**
+     * Sets the number of the center node column.
+     * @param columnNumber The number that represents a column in the CSV file, which relates to center nodes.
+     */
     public void setCenterNodeColumnNumber( int columnNumber ) {
         this.centerNodeColumnNumber = columnNumber;
     }
 
+    /**
+     * Sets the list of information node column numbers.
+     * @param columnNumbers An array of integers that represent CSV column numbers, which relate to information nodes.
+     */
     public void setInformationNodeColumnNumbers( int[] columnNumbers ) {
         this.informationNodeColumnNumbers = columnNumbers;
     }
 
+    /**
+     * Sets the mapping of column numbers to headers.
+     * @param columnToNameMapping A {@link HashMap} with a key of an integer, representing a column number in the CSV
+     *                            file, and a value of a String, representing the name of that column.
+     */
     public void setColumnToNameMapping( HashMap<Integer, String> columnToNameMapping ) {
         this.columnToColumnNameMapping = columnToNameMapping;
+    }
+
+    /**
+     * @param data The column name we want to pull data from to populate a center node.
+     */
+    public GenericTreeNode<String> loadAbsoluteParent( Object data ) throws BadSetupException {
+        if( centerNodeColumnNumber == -1
+            || informationNodeColumnNumbers.length == 0
+            || columnToColumnNameMapping.isEmpty() ) {
+            throw new BadSetupException( "You must provide a valid center node column number, information node column "
+                                         + "numbers, and/or valid column names to run a CSV EntityG instance." );
+        }
+
+        String s = (String) data;
+        if( !columnToColumnNameMapping.containsKey( centerNodeColumnNumber ) ) {
+            throw new BadSetupException( "The column number '" + centerNodeColumnNumber + "' does not exist in the "
+                                         + "CSV file." );
+        } else if( !columnToColumnNameMapping.get( centerNodeColumnNumber ).equals( s ) ) {
+            throw new BadSetupException( "The column number '" + centerNodeColumnNumber + "' does not match the label '"
+                                         + s + "'. Please provide the correct column number which matches '"
+                                         + s + "'." );
+        }
+
+        try {
+            //Get the first CSV row of actual data (not the header row)
+            TableRow firstLine = conn.getLine();
+            //Get the data in the center node column (specified by a column number)
+            String centerData = firstLine.getColumn( centerNodeColumnNumber );
+            GenericTreeNode<String> rootParent = new GenericTreeNode<String>( true, centerData, s );
+
+            for( int i = 0; i < informationNodeColumnNumbers.length; i++ ) {
+                int infoNum = informationNodeColumnNumbers[i];
+                rootParent.addChild( new GenericTreeNode<String>( false, firstLine.getColumn( infoNum ),
+                                                                  columnToColumnNameMapping.get( infoNum ) ) );
+            }
+            return rootParent;
+        } catch( IOException ioe ) {
+            throw new BadSetupException( "There was an error trying to receieve data from the csv file." );
+        }
+    }
+
+    /**
+     * @param data The data that is contained in <code>parent</code>. <code>parent</code> will be a center node.
+     */
+    public GenericTreeNode<String> loadInformationNodes( GenericTreeNode<String> parent, Object... data )
+            throws BadSetupException {
+        String centerNodeData = (String) data[0];
+
+        try {
+            TableRow row = null;
+            while( (row = conn.getLine()) != null ) {
+                if( row.getColumn( centerNodeColumnNumber ).equals( centerNodeData ) ) {
+                    break;
+                } else {
+                    row = null;
+                }
+            }
+
+            if( row != null ) {
+                for( int i = 0; i < informationNodeColumnNumbers.length; i++ ) {
+                    int infoNum = informationNodeColumnNumbers[i];
+                    parent.addChild( new GenericTreeNode<String>( false, row.getColumn( infoNum ),
+                                                                  columnToColumnNameMapping.get( infoNum ) ) );
+                }
+            } else {
+                ExceptionUtils.handleMessage( "No information for center node: " + parent.toString() );
+            }
+            conn.reset();
+            return parent;
+        } catch( IOException ioe ) {
+            throw new BadSetupException( "There was a problem retrieving information from the csv file." );
+        }
+    }
+
+    /**
+     * @param data Must be two strings: The data of the information node that the user clicked on, and that piece of
+     *             data's column name.
+     */
+    public GenericTreeNode<String> loadCenterNodes( GenericTreeNode<String> parent, int maxNodes, Object... data )
+            throws BadSetupException {
+        String informationNodeData = (String) data[0];
+        String informationColumnHeader = (String) data[1];
+
+        int column = -1;
+        for( int i = 0; i < informationNodeColumnNumbers.length; i++ ) {
+            if( columnToColumnNameMapping.get( informationNodeColumnNumbers[i] ).equals( informationColumnHeader ) ) {
+                column = informationNodeColumnNumbers[i];
+            }
+        }
+
+        try {
+            TableRow row = null;
+            while( (row = conn.getLine()) != null ) {
+                if( row.getColumn( column ).equals( informationNodeData ) ) {
+                    parent.addChild(
+                            new GenericTreeNode<String>( true, row.getColumn( centerNodeColumnNumber ),
+                                                         columnToColumnNameMapping.get( centerNodeColumnNumber ) ) );
+                }
+            }
+            conn.reset();
+            return parent;
+        } catch( IOException ioe ) {
+            throw new BadSetupException( "There was a problem retrieving information from the csv file." );
+        }
     }
 }
